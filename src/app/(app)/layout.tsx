ed6bad1c27aa53b14db7a5445703ad1fd6626c1a
Winder Perez <Basthiancasparod@gmail.com>
2026-06-06 12:31:05 -0400

    fix: build fails on configuracion (next-intl prerender) - add plugin + NextIntlClientProvider in layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,16 @@
+import { NextIntlClientProvider } from 'next-intl'
 import { SupabaseProvider } from '@/components/layout/SupabaseProvider'
 import { ThemeProvider } from '@/components/layout/ThemeProvider'
 import { AppShell } from '@/components/layout/AppShell'
+import es from '../../../messages/es.json'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SupabaseProvider>
       <ThemeProvider>
-        <AppShell>{children}</AppShell>
+        <NextIntlClientProvider locale="es" messages={es} timeZone="America/Mexico_City">
+          <AppShell>{children}</AppShell>
+        </NextIntlClientProvider>
       </ThemeProvider>
     </SupabaseProvider>
   )
